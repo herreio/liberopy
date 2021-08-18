6e1473a2a641206dfdb2d9291dcc7cf08d170866
herre <donatus.herre@slub-dresden.de>
2021-08-18 21:56:36 +0200

    Bump version number to tomorrow

--- liberopy/__init__.py
+++ liberopy/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "2021.8.18"
+__version__ = "2021.8.19"
 __license__ = "GPLv3"
 
 from .webservices import WebServices
