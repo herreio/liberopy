f9a4137c6fac80fd06ab9334880346ab936d67c5
herre <donatus.herre@slub-dresden.de>
2021-08-25 10:49:58 +0200

    Bump version number

--- liberopy/webservices.py
+++ liberopy/webservices.py
@@ -94,7 +94,7 @@ class ServicePackage:
 
     def get_request(self, url):
         try:
-            response = requests.get(url, headers={"User-Agent": "liberopy 2021.8.21"})
+            response = requests.get(url, headers={"User-Agent": "liberopy 2021.8.25"})
         except requests.exceptions.RequestException as e:
             self.logger.error(e.__class__.__name__)
             return None
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="liberopy",
-    version="2021.8.21",
+    version="2021.8.25",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="LIBERO Web Services API Client",
