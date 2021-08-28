09662fc70e8f49b5c298c360c03499b1e92ae369
herre <donatus.herre@slub-dresden.de>
2021-08-28 16:37:05 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import setuptools
 
 setuptools.setup(
     name="liberopy",
-    version="2021.8.25",
+    version="2021.8.28",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="LIBERO Web Services API Client",
     license=open("LICENSE").read(),
     url="https://github.com/herreio/liberopy",
     packages=["liberopy"],
-    install_requires=["requests", "lxml"],
+    install_requires=["requests", "lxml", "python-dateutil"],
 )
