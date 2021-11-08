5480cee3f000df528efeafe25ab4c75f1804b6a5
herre <donatus.herre@slub-dresden.de>
2021-11-08 08:33:00 +0100

    Bump version number

--- liberopy/webservices.py
+++ liberopy/webservices.py
@@ -430,7 +430,7 @@ class ServicePackage:
 
     def get_request(self, url):
         try:
-            response = requests.get(url, headers={"User-Agent": "liberopy 2021.10.28"})
+            response = requests.get(url, headers={"User-Agent": "liberopy 2021.11.8"})
         except requests.exceptions.RequestException as e:
             self.logger.error(e.__class__.__name__)
             return None
