44a12391aef70cd0f2fe51d1cd37ce17c284f3cb
Donatus Herre <donatus.herre@slub-dresden.de>
2022-06-04 23:48:47 +0200

    Bump version number

--- liberopy/__init__.py
+++ liberopy/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "2022.6.4"
+__version__ = "2022.6.5"
 __license__ = "GPLv3"
 
 from . import xmlparser
