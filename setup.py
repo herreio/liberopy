1f575872a22697850e23fd778b0d66aaaf7688d5
Donatus Herre <donatus.herre@slub-dresden.de>
2022-08-21 13:20:12 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="liberopy",
-    version="2022.8.20",
+    version="2022.8.21",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="LIBERO Web Services SOAP API Client",
