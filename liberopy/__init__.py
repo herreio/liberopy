d0c9ad3925f63303a0f67195b2aef50ed6fefdc1
Donatus Herre <donatus.herre@slub-dresden.de>
2022-09-11 14:24:01 +0200

    Bump version number

--- liberopy/__init__.py
+++ liberopy/__init__.py
@@ -4,7 +4,7 @@
 """
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "2022.8.23"
+__version__ = "2022.9.11"
 __license__ = "GPLv3"
 
 from . import xmlparser
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="liberopy",
-    version="2022.8.23",
+    version="2022.9.11",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="LIBERO Web Services SOAP API Client",
